9d9c5b2e0fd0d09d57aa3dc86b82a8b2210c07ce
Brian <breen.mac.cana@gmail.com>
2016-01-26 17:55:36 +0000

    tutorial continued

--- site/js/Tutorial.js
+++ site/js/Tutorial.js
@@ -3,16 +3,69 @@ var viewModel = {
 	currentPageName : ko.observable("Tutorial"),
 	menuOptions : ko.observableArray([{name:"Tutorial",url:"Tutorial.html",_class:"active"},
 									{name:"Home",url:"home.html",_class:""},
-									{name:"LogOut",url:"LogOut.html",_class:""},]),
-}
+									{name:"LogOut",url:"LogOut.html",_class:""},]), 
+	Finish : function(){
+		document.location.href = "home.html";
+	},
+	NewGameNameError: ko.observableArray(),
+	NewGameTimeError : ko.observableArray(),
+	fishingGold : ko.observable("50"),
+	fishingRange : ko.observable("100"),
+	hammerGold : ko.observable("50"),
+	hammerRange : ko.observable("100"),
+	diggerGold : ko.observable("50"),
+	diggerRange : ko.observable("100"),
+	thievesGold : ko.observable("50"),
+	thievesRange : ko.observable("100"),
+	bookGold : ko.observable("50"),
+	bookRange : ko.observable("100"),
+	basketGold : ko.observable("50"),
+	basketRange : ko.observable("100"),
+	pocketGold : ko.observable("50"),
+	pocketRange : ko.observable("100"),
+	chainGold : ko.observable("50"),
+	chainRange : ko.observable("100"),
+};
 
 var myLatLng;  
 
+var setUpGameTut = 1, playGameTut = 2;
 
 
+var currentStep = 1;
+var currentSubStep = 1;
+var currentTutorial = setUpGameTut;
 
 /* Function gets called when page loaded */
 $(document).ready(function(){
+
+	/*highlight the textinput for step 1*/
+	$("#NewGameName").effect( "highlight", {color:"#008000"}, 5000 );
+	/*Detect change to textinput for step 1*/
+	$('#NewGameName').on('input',function(e){
+		/*Only call next when there is change to name and current step is 1*/
+		if (currentStep == 1)
+		{
+			next();
+		}
+	});
+	/*Detect change to textinput for step 2*/
+	$('#NewgameEndTime').on('input',function(e){ 
+		/*Only call next when there is change to name and current step is 1*/
+		if (currentStep == 2)
+		{
+			next();	
+		}
+	});
+	/*Detect change to textinput for step 3*/
+	$('.carousel-control').click(function(e){ 
+		/*Only call next when there is change to name and current step is 1*/
+		if (currentStep == 3 && currentSubStep == 1)
+		{
+			next();	
+		}
+	});
+	
 
 });//end doc ready
 
@@ -112,6 +165,71 @@ function showError(error) {
     }
 }
 
+function next()
+{
+	//move the substep forward
+	currentSubStep += 1;
+	if(currentTutorial == setUpGameTut)
+	{
+		if(currentStep == 1)
+		{
+			currentStep +=1; 
+			currentSubStep = 1;
+			/*highlight the textinput for step 2*/
+			$("#NewgameEndTime").effect( "highlight", {color:"#008000"}, 5000 );
+		}
+		else if(currentStep == 3)
+		{
+			if(currentSubStep >= 6)
+			{//the step is complete move to next step and reset sub step
+				currentStep +=1; 
+				currentSubStep = 1;
+			}
+		}
+		else if (currentStep == 4)
+		{
+			currentTutorial = playGameTut;
+			currentStep = 1;
+			currentSubStep = 1;
+			$("#SetUpGameTut").hide();
+			$("#PlayGameTut").show();
+		}
+		else
+		{
+			currentStep +=1; 
+			currentSubStep = 1;
+		}
+	}
+	else if(currentTutorial == playGameTut)
+	{
+		if(currentStep == 1)
+		{
+			if(currentSubStep >= 3)
+			{//the step is complete move to next step and reset sub step
+				currentStep +=1; 
+				currentSubStep = 1;
+			}
+		}
+		else if (currentStep == 2)
+		{
+			if(currentSubStep >= 7)
+			{//the step is complete move to next step and reset sub step
+				currentStep +=1; 
+				currentSubStep = 1;
+			}
+		}
+	}
+	//hide all 
+	for (var i = 0 ; i < 6; i++ )
+	{
+		$(".step" + i).hide();
+		$(".substep" + i).hide();
+	}
+	//show the current step 
+	$(".step" + currentStep).show();
+	$(".substep" + currentSubStep).show();
+	
+}
 
 
 
